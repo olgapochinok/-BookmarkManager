a56ca1257900515b908a0e1273fdcdd10f9cc334
Olga Pochinok <o.pochinok@yandex.ru>
2026-09-03 17:35:43 +0300

    Создание внешнего вида закладок в виде колонок

--- src/modules/MainContent.jsx
+++ src/modules/MainContent.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import { useBookmarks } from "../BookmarksContext";
+import styles from "./MainContent.module.scss";
 
 function BookmarkCol({ bookmark, favicon }) {
+  const [isSelect, setIsSelect] = useState(false);
+
+  function clickBookmark(event) {
+    event.preventDefault();
+    setIsSelect((hasSelect) => !hasSelect);
+  }
+
   return (
-    <div>
-      <img src={favicon} />
-      <a href={bookmark.url}>
+    <div
+      className={`
+      ${styles["bookmark-col"]}
+      ${isSelect ? styles["bookmark-col--select"] : ""}`}
+    >
+      <button className={styles["bookmark-col__btn"]}>
+        <img src={favicon} />
+      </button>
+      <a
+        className={styles["bookmark-col__link"]}
+        onClick={clickBookmark}
+        href={bookmark.url}
+      >
         <span>{bookmark.title || "Без названия"}</span>
       </a>
     </div>
@@ -13,17 +32,16 @@ function BookmarkCol({ bookmark, favicon }) {
 
 export default function MainContent() {
   const { currentBookmarks } = useBookmarks();
+  
   function getFaviconUrl(pageUrl) {
     const url = new URL(chrome.runtime.getURL("/_favicon/"));
     url.searchParams.set("pageUrl", pageUrl);
     url.searchParams.set("size", "16"); // Размер иконки (16, 32, 64)
     return url.toString();
   }
+  
   return (
-    <div className="content">
-      {currentBookmarks.length && currentBookmarks.length === 0 ? (
-        <p>В этой папке нет прямых ссылок</p>
-      ) : null}
+    <div className="content" style={{ columnCount: 4 }}>
       {currentBookmarks.map((item) => (
         <BookmarkCol
           key={item.id}
