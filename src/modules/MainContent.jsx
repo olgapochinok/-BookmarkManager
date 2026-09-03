069b75d8e83037c403a0968fc3cac05331b85801
Olga Pochinok <o.pochinok@yandex.ru>
2026-09-03 12:45:50 +0300

    Добавление favicon к сылкам

--- src/modules/MainContent.jsx
+++ src/modules/MainContent.jsx
@@ -1,19 +1,35 @@
 import { useBookmarks } from "../BookmarksContext";
 
+function BookmarkCol({ bookmark, favicon }) {
+  return (
+    <div>
+      <img src={favicon} />
+      <a href={bookmark.url}>
+        <span>{bookmark.title || "Без названия"}</span>
+      </a>
+    </div>
+  );
+}
+
 export default function MainContent() {
   const { currentBookmarks } = useBookmarks();
+  function getFaviconUrl(pageUrl) {
+    const url = new URL(chrome.runtime.getURL("/_favicon/"));
+    url.searchParams.set("pageUrl", pageUrl);
+    url.searchParams.set("size", "16"); // Размер иконки (16, 32, 64)
+    return url.toString();
+  }
   return (
     <div className="content">
       {currentBookmarks.length && currentBookmarks.length === 0 ? (
         <p>В этой папке нет прямых ссылок</p>
       ) : null}
       {currentBookmarks.map((item) => (
-        <div key={item.id}>
-          <a href={item.url}>
-            <span>{item.title || "Без названия"}</span>
-            <span> {item.url}</span>
-          </a>
-        </div>
+        <BookmarkCol
+          key={item.id}
+          bookmark={item}
+          favicon={getFaviconUrl(item.url)}
+        ></BookmarkCol>
       ))}
     </div>
   );
